5f7ff1428c573dca3cbb79e17763037ff68e21a7
SchuwiProfessional <alvaxxx170375@gmail.com>
2023-05-09 15:22:23 -0500

    Initial Commit

---
+++ SwiftInventory/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftInventory
+//
+//  Created by Alvaro  on 9/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftInventory/SwiftInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftInventoryApp.swift
+//  SwiftInventory
+//
+//  Created by Alvaro  on 9/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
